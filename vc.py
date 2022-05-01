a247e0dd8b9d28973b82dcd0082f1c8215d7c169
Nikhil <nikhilrana884@gmail.com>
2022-05-01 22:08:12 +0530

    frame capture cv2

---
+++ vc.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('video.mp4')
+
+while True:
+    ret,frame1 = cap.read()
+
+    cv2.imshow('Video Original',frame1)
+
+    if cv2.waitKey(1) == 13:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
