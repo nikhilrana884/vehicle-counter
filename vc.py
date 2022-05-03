505b62c261f37203db272349259c2be944b596a4
Nikhil <nikhilrana884@gmail.com>
2022-05-03 23:45:30 +0530

    algo added

--- vc.py
+++ vc.py
@@ -3,10 +3,24 @@ import numpy as np
 
 cap = cv2.VideoCapture('video.mp4')
 
+algo = cv2.createBackgroundSubtractorMOG2()
+
 while True:
     ret,frame1 = cap.read()
+    grey = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(grey, (3,3), 5)
+    
+    img_sub = algo.apply(blur)
+    dilat = cv2.dilate(img_sub, np.ones((5,5)))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    dilatada = cv2.morphologyEx(dilat,cv2.MORPH_CLOSE,kernel)
+    dilatada = cv2.morphologyEx(dilatada,cv2.MORPH_CLOSE,kernel)
+    counter = cv2.findContours(dilatada, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.imshow('Video Original',frame1)
+
+    cv2.imshow('Detector',dilatada)
+
+    #cv2.imshow('Video Original', frame1)
 
     if cv2.waitKey(1) == 13:
         break
